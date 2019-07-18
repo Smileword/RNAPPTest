b8498428e1319282571fe9c7f1ced930b49fedf7
mayongfeng <709957271@qq.com>
2019-07-18 10:56:52 +0800

    iOS集成RN 测试项目导入 npm start xcode 运行

---
+++ iOSRN/NumberTileGame/NumberTileGame/F3HViewController.m
@@ -0,0 +1,52 @@
+//
+//  F3HViewController.m
+//  NumberTileGame
+//
+//  Created by Austin Zheng on 3/22/14.
+//
+//
+
+#import "F3HViewController.h"
+#import <React/RCTRootView.h>
+#import "F3HNumberTileGameViewController.h"
+
+@interface F3HViewController ()
+@end
+
+@implementation F3HViewController
+
+- (IBAction)playGameButtonTapped:(id)sender {
+    F3HNumberTileGameViewController *c = [F3HNumberTileGameViewController numberTileGameWithDimension:4
+                                                                                         winThreshold:2048
+                                                                                      backgroundColor:[UIColor whiteColor]
+                                                                                          scoreModule:YES
+                                                                                       buttonControls:NO
+                                                                                        swipeControls:YES];
+    [self presentViewController:c animated:YES completion:nil];
+}
+- (IBAction)test:(id)sender {
+    NSLog(@"High Score Button Pressed");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                moduleName: @"RNHighScores"
+                         initialProperties:
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"Alex",
+                   @"value": @"42"
+                   },
+               @{
+                   @"name" : @"Joel",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                             launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+@end
